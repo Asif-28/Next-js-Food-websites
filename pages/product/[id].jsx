dd1a86ec32321aba76a6f83fcd9cf9083367364c
Asif Ali <asif.a.7277@gmail.com>
2022-10-03 16:56:54 +0530

    completed the products section

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,12 +2,12 @@ import styles from "../../styles/Product.module.css";
 import Image from "next/image";
 import { useState } from "react";
 export const Product = () => {
-  const [size, setSize] = useState(2);
+  const [size, setSize] = useState(0);
   const foodItem = {
     id: 1,
-    img: "/img/f1.jpg",
+    img: "/img/f2.jpg",
     name: " Our Special ",
-    price: " 199, 277, 499",
+    price: [199, 299, 499],
     desc: "this is the special item avialable in out fooding corner ",
   };
   return (
@@ -23,17 +23,38 @@ export const Product = () => {
         <p className={styles.desc}>{foodItem.desc}</p>
         <h3 className={styles.choose}>Choose the item quantity</h3>
         <div className={styles.sizes}>
-          <div className={styles.size}>
+          <div className={styles.size} onClick={() => setSize(0)}>
             {/* <Image src="/img/size.png" alt="" layout="fill" /> */}
             <button className={styles.btn}>Small</button>
           </div>
           <div className={styles.size}>
             {/* <Image src="/img/size.png" alt="" layout="fill" /> */}
-            <button className={styles.btn}>Medium</button>
+            <button className={styles.btn} onClick={() => setSize(1)}>
+              Medium
+            </button>
+            <span className={styles.number}>Best Deal!!</span>
           </div>
           <div className={styles.size}>
-            <button className={styles.btn}>Large</button>
+            <button className={styles.btn} onClick={() => setSize(2)}>
+              Large
+            </button>
           </div>
+        </div>
+        {/* <h3 className={styles.choose}>Choose The packing option </h3>
+        <div className={styles.add}>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              id="check"
+              name=""
+              className={styles.checkbox}
+            />
+          </div>
+          <label htmlFor="check"> Environmental Friendly </label>
+        </div> */}
+        <div className={styles.add}>
+          <input type="number" defaultValue={1} className={styles.quantity} />
+          <button className={styles.btn}>Add to cart</button>
         </div>
       </div>
     </div>
